2d18f386bbd5a376c29323d643a9fd6e4dffc237
Sparsh Gupta <44977909+sparshgupta2212@users.noreply.github.com>
2018-11-30 19:31:24 +0530

    Add files via upload

---
+++ pattern 1.c.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+   for (a=1;a<=5;a++)
+   {
+        for (b=1;b<=a;b++)
+        
+            printf("* ");
+        
+      printf("\n");
+    }
+}
---
+++ pattern 2.c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   for (a=1;a<=5;a++)
+   {
+      for (b=1;b<a;b++)
+      {
+         printf(" ");
+      }
+      for (c=5;c>=a;c--)
+      {
+         printf("*");
+      }
+      printf("\n");
+   }
+}
